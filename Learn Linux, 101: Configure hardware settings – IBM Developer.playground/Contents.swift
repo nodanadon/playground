262f695ce40cfc0b5450649e35984eea4e5e8bdf
nodanadon <knoda0412@yahoo.co.jp>
2019-07-28 00:33:39 +0900

    First create

---
+++ Learn Linux, 101: Configure hardware settings – IBM Developer.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+print("Hello to the world")
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "The Width is "
+let width = 94
+let widthlabel = label + String(width)
+
+print(widthlabel)
+
