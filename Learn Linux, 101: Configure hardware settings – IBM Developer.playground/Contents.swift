e4920b65717dfc9faceabdc971e31bff614861f7
nodanadon <knoda0412@yahoo.co.jp>
2019-07-28 22:05:26 +0900

    class and reference

--- Learn Linux, 101: Configure hardware settings – IBM Developer.playground/Contents.swift
+++ Learn Linux, 101: Configure hardware settings – IBM Developer.playground/Contents.swift
@@ -1,5 +1,88 @@
 import UIKit
 
+class Shape {
+    var numberOfSides = 0
+    func simpleDescrption() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+    
+}
+
+class Square: NamedShape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)."
+    }
+    
+}
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+
+class TrianglAndSquare {
+    var triangle: EquilateralTriangle {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+
 /* Function */
 func greet(person: String, day: String) ->
     String {
@@ -215,3 +298,20 @@ print(increment(7))
 
 var numbers = [20, 19, 17, 12]
 hasAnyMatches(list: numbers, condition: lessThanTen)
+
+
+/* Objects and Classes session */
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescrption()
+print(shapeDescription)
+
+var namedShape = NamedShape(name: "Taro")
+print(namedShape.simpleDescription())
+
+let test = Square(sideLength: 5.2, name: "my test squaare")
+test.area()
+print(test.simpleDescription())
+print(test.area())
+
+
