dd1a84681e255e9175659d105eb066bd8e2531e4
nodanadon <knoda0412@yahoo.co.jp>
2019-07-30 21:45:56 +0900

    Error process

--- Learn Linux, 101: Configure hardware settings – IBM Developer.playground/Contents.swift
+++ Learn Linux, 101: Configure hardware settings – IBM Developer.playground/Contents.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
 /* Enumaration */
 enum Rank: Int {
     case ace = 1
@@ -208,6 +214,21 @@ func lessThanTen(number: Int) -> Bool {
     return number < 10
 }
 
+/* Protocol and Extentions */
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class"
+    var anotherPriperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted"
+    }
+}
+
+
 var str = "Hello, playground"
 print("Hello to the world")
 
@@ -406,3 +427,46 @@ case let .failure(message):
 
 let threeOfSpades = Card(rank: .three, suit: .spades)
 let threeOfSpadeDescription = threeOfSpades.simpleDescriotion()
+
+var a = SimpleClass()
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+do{
+    let printerResponse = try send(job: 1040, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try send(job: 1440, toPrinter: "onFire")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
